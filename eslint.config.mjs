ff48aaab2b69d03184bf8605ecdb1679e18e64f9
Trần Minh Nhựt <nhut.9dev@gmail.com>
2025-02-24 21:50:05 +0700

    feat: init and setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.plugins('prettier'),
+  {
+    rules: {
+      semi: ['error', 'always'], // Luôn có dấu chấm phẩy
+      quotes: ['error', 'single'], // Dùng dấu nháy đơn thay vì nháy kép
+      'no-console': 'warn', // Cảnh báo nếu dùng console.log
+      'prettier/prettier': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
